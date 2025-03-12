e8ca18604300505871d0adb920118ee69917fe2e
koolaitman <kaytelintelo@icloud.com>
2025-03-12 11:21:47 +0100

    stair fix

---
+++ Project-Innovation/Test Gyro/Assets/audioManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class audioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
